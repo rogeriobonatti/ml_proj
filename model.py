2310dcb7dbf16b392a05c87a0d44362d6184cc3f
Rogerio Bonatti <rbonatti@GS16698.SP.CS.CMU.EDU>
2017-04-14 18:52:21 -0400

    new network

--- model.py
+++ model.py
@@ -6,11 +6,11 @@ import cv2, numpy as np
 
 def VGG_16(weights_path=None):
     model = Sequential()
-    model.add(ZeroPadding2D((1,1),input_shape=(3,224,224)))
-    model.add(Convolution2D(64, 3, 3, activation='relu'))
-    model.add(ZeroPadding2D((1,1)))
-    model.add(Convolution2D(64, 3, 3, activation='relu'))
-    model.add(MaxPooling2D((2,2), strides=(2,2)))
+    model.add(ZeroPadding2D((1,1),input_shape=(3,224,224), name='conv1_pad1'))
+    model.add(Convolution2D(64, 3, 3, activation='relu',name='conv1_act1'))
+    model.add(ZeroPadding2D((1,1),name='conv1_pad2'))
+    model.add(Convolution2D(64, 3, 3, activation='relu',name='conv1_act2'))
+    model.add(MaxPooling2D((2,2), strides=(2,2),name='conv1_max'))
 
     model.add(ZeroPadding2D((1,1)))
     model.add(Convolution2D(128, 3, 3, activation='relu'))
@@ -29,7 +29,7 @@ def VGG_16(weights_path=None):
     model.add(ZeroPadding2D((1,1)))
     model.add(Convolution2D(512, 3, 3, activation='relu'))
     model.add(ZeroPadding2D((1,1)))
-    model.add(Convolution2D(512, 3, 3, activation='relu'))
+    model.add(Convolution2D(512, 3, 3, activation='relu'))r
     model.add(ZeroPadding2D((1,1)))
     model.add(Convolution2D(512, 3, 3, activation='relu'))
     model.add(MaxPooling2D((2,2), strides=(2,2)))
@@ -48,6 +48,8 @@ def VGG_16(weights_path=None):
     model.add(Dense(4096, activation='relu'))
     model.add(Dropout(0.5))
     model.add(Dense(1000, activation='softmax'))
+
+    model.summary()
 
     if weights_path:
         model.load_weights(weights_path)
