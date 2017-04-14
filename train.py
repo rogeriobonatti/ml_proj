a06369a2a1c0ec9ac2df9360d7b149cdbccbfe8e
Rogerio Bonatti <rbonatti@GS16698.SP.CS.CMU.EDU>
2017-04-14 19:19:18 -0400

    new network

--- train.py
+++ train.py
@@ -1,6 +1,15 @@
-from keras.optimizers import Adam
-
 import model
+from keras.optimizers import Adam
+from keras.preprocessing.image import ImageDataGenerator
+
+
+img_width, img_height = 224, 224
+train_data_dir = '/data/datasets/rbonatti/data_processed/1/train80'
+validation_data_dir = '/data/datasets/rbonatti/data_processed/1/train20'
+nb_train_samples = 7998*2
+nb_validation_samples = 2002*2
+epochs = 50
+batch_size = 16
 
 if __name__ == "__main__":
 
@@ -9,4 +18,26 @@ if __name__ == "__main__":
     adam = Adam(lr=0.001, beta_1=0.9, beta_2=0.999, epsilon=1e-08, decay=0.0)
     network.compile(optimizer=adam, loss='categorical_crossentropy')
 
-    
+    # prepare data augmentation configuration
+    datagen = ImageDataGenerator(rescale=1. / 255)
+
+    train_generator = datagen.flow_from_directory(
+        train_data_dir,
+        target_size=(img_height, img_width),
+        batch_size=batch_size,
+        class_mode='binary')
+
+    validation_generator = datagen.flow_from_directory(
+        validation_data_dir,
+        target_size=(img_height, img_width),
+        batch_size=batch_size,
+        class_mode='binary')
+
+    # fine-tune the model
+    model.fit_generator(
+        train_generator,
+        samples_per_epoch=nb_train_samples,
+        epochs=epochs,
+        validation_data=validation_generator,
+        nb_val_samples=nb_validation_samples)
+        
