b93a750ea0c1cf0436b6cb5005b82b13663fa1e1
Rogerio Bonatti <rbonatti@GS16698.SP.CS.CMU.EDU>
2017-04-14 18:49:03 -0400

    new network

--- train.py
+++ train.py
@@ -7,3 +7,4 @@ if __name__ == "__main__":
     network = model.VGG_16('/data/datasets/rbonatti/vgg16_weights.h5')
     sgd = SGD(lr=0.1, decay=1e-6, momentum=0.9, nesterov=True)
     network.compile(optimizer=sgd, loss='categorical_crossentropy')
+    print "YEY"
