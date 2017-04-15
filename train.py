9bbf62d75b71f45812d0f73cd278c6fbd2f58e30
Rogerio Bonatti <rbonatti@GS16698.SP.CS.CMU.EDU>
2017-04-14 20:32:37 -0400

    new network

--- train.py
+++ train.py
@@ -16,7 +16,7 @@ if __name__ == "__main__":
     # Test pretrained model
     network = model.VGG_16('/data/datasets/rbonatti/vgg16_weights_with_name.h5')
     adam = Adam(lr=0.001, beta_1=0.9, beta_2=0.999, epsilon=1e-08, decay=0.0)
-    network.compile(optimizer=adam, loss='binary_crossentropy')
+    network.compile(optimizer=adam, loss='binary_crossentropy', metrics=['accuracy'])
 
     # prepare data augmentation configuration
     datagen = ImageDataGenerator(rescale=1. / 255)
