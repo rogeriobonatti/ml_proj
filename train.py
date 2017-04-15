3378c947d6fde521507d55ecf6e07ee6b042f4fb
Rogerio Bonatti <rbonatti@GS16698.SP.CS.CMU.EDU>
2017-04-14 23:50:30 -0400

    new network

--- train.py
+++ train.py
@@ -7,7 +7,9 @@ from keras.callbacks import ModelCheckpoint
 img_width, img_height = 224, 224
 train_data_dir = '/data/datasets/rbonatti/data_processed/1/train80'
 validation_data_dir = '/data/datasets/rbonatti/data_processed/1/train20'
-nb_train_samples = 7998*2
+save_models_dir= '/data/datasets/rbonatti/ml_weights/'
+# nb_train_samples = 7998*2
+nb_train_samples = 100
 nb_validation_samples = 2002*2
 epochs = 50
 batch_size = 16
@@ -41,7 +43,11 @@ if __name__ == "__main__":
     # plot_loss_callback = LambdaCallback(on_epoch_end=lambda epoch, logs: plt.plot(np.arange(epoch),logs['loss']))
     # batch_print_callback = LambdaCallback(on_batch_begin=lambda batch,logs: print(batch))
     # saves the model weights after each epoch if the validation loss decreased
-    checkpointer = ModelCheckpoint(filepath="/data/datasets/rbonatti/ml_weights/weights.{epoch:02d}-{val_loss:.2f}.hdf5", verbose=1, save_best_only=True)
+    checkpointer = ModelCheckpoint(filepath="/data/datasets/rbonatti/ml_weights/weights.{epoch:02d}-{val_loss:.2f}.hdf5", verbose=1)
+
+    # folder to save the weights
+    if not os.path.exists(save_models_dir):
+    os.makedirs(save_models_dir)
 
     # fine-tune the model
     network.fit_generator(
