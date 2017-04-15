c50d73916a49d98c80773a2ac009935d63bc0843
Rogerio Bonatti <rbonatti@GS16698.SP.CS.CMU.EDU>
2017-04-15 00:17:28 -0400

    new network

--- eval.py
+++ eval.py
@@ -15,19 +15,19 @@ if __name__ == "__main__":
 	adam = Adam(lr=0.001, beta_1=0.9, beta_2=0.999, epsilon=1e-08, decay=0.0)
 	network.compile(optimizer=adam, loss='binary_crossentropy', metrics=['accuracy'])
 
-    # prepare data augmentation configuration
-    datagen = ImageDataGenerator(rescale=1. / 255)
+	# prepare data augmentation configuration
+	datagen = ImageDataGenerator(rescale=1. / 255)
 
-    valid_generator = datagen.flow_from_directory(
-        valid_data_dir,
-        target_size=(img_height, img_width),
-        batch_size=batch_size,
-        class_mode='binary',
-        color_mode='grayscale')
+	valid_generator = datagen.flow_from_directory(
+	    valid_data_dir,
+	    target_size=(img_height, img_width),
+	    batch_size=batch_size,
+	    class_mode='binary',
+	    color_mode='grayscale')
 
-    predictions=network.predict_generator(
-        generator=valid_generator,
-        validation_data=validation_generator
-        )
+	predictions=network.predict_generator(
+	    generator=valid_generator,
+	    validation_data=validation_generator
+	    )
 
-    np.savetxt('/data/datasets/rbonatti/ml_prediction.out', predictions, delimiter=',')
+	np.savetxt('/data/datasets/rbonatti/ml_prediction.out', predictions, delimiter=',')
