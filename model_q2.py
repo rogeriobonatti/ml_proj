64b75ac6cadd0030898e8e1f20a7e731a52fd493
Rogerio Bonatti <rbonatti@GS16698.SP.CS.CMU.EDU>
2017-04-16 17:29:59 -0400

    new files

---
+++ model_q2.py
@@ -0,0 +1,91 @@
+from keras.models import Sequential
+from keras.layers.core import Flatten, Dense, Dropout
+from keras.layers.convolutional import Convolution2D, MaxPooling2D, ZeroPadding2D
+from keras.optimizers import SGD
+from keras.layers import Activation
+from keras.layers.normalization import BatchNormalization
+import cv2, numpy as np
+
+def VGG_16(weights_path=None):
+    model = Sequential()
+    model.add(ZeroPadding2D((1,1), input_shape=(1,224,224), name='conv1_pad1_finetune'))
+    model.add(Convolution2D(64, 3, 3,name='conv1_act1_finetune'))
+    model.add(BatchNormalization(axis=1))
+    model.add(Activation('relu'))
+    model.add(ZeroPadding2D((1,1),name='conv1_pad2'))
+    model.add(Convolution2D(64, 3, 3,name='conv1_act2'))
+    model.add(BatchNormalization(axis=1))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D((2,2), strides=(2,2),name='conv1_max'))
+
+    model.add(ZeroPadding2D((1,1),name='conv2_pad1'))
+    model.add(Convolution2D(128, 3, 3,name='conv2_act1'))
+    model.add(BatchNormalization(axis=1))
+    model.add(Activation('relu'))
+    model.add(ZeroPadding2D((1,1),name='conv2_pad2'))
+    model.add(Convolution2D(128, 3, 3,name='conv2_act2'))
+    model.add(BatchNormalization(axis=1))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D((2,2), strides=(2,2),name='conv2_max'))
+
+    model.add(ZeroPadding2D((1,1),name='conv3_pad1'))
+    model.add(Convolution2D(256, 3, 3,name='conv3_act1'))
+    model.add(BatchNormalization(axis=1))
+    model.add(Activation('relu'))
+    model.add(ZeroPadding2D((1,1),name='conv3_pad2'))
+    model.add(Convolution2D(256, 3, 3,name='conv3_act2'))
+    model.add(BatchNormalization(axis=1))
+    model.add(Activation('relu'))
+    model.add(ZeroPadding2D((1,1),name='conv3_pad3'))
+    model.add(Convolution2D(256, 3, 3,name='conv3_act3'))
+    model.add(BatchNormalization(axis=1))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D((2,2), strides=(2,2),name='conv3_max'))
+
+    model.add(ZeroPadding2D((1,1),name='conv4_pad1'))
+    model.add(Convolution2D(512, 3, 3,name='conv4_act1'))
+    model.add(BatchNormalization(axis=1))
+    model.add(Activation('relu'))
+    model.add(ZeroPadding2D((1,1),name='conv4_pad2'))
+    model.add(Convolution2D(512, 3, 3,name='conv4_act2'))
+    model.add(BatchNormalization(axis=1))
+    model.add(Activation('relu'))
+    model.add(ZeroPadding2D((1,1),name='conv4_pad3'))
+    model.add(Convolution2D(512, 3, 3,name='conv4_act3'))
+    model.add(BatchNormalization(axis=1))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D((2,2), strides=(2,2),name='conv4_max'))
+
+    model.add(ZeroPadding2D((1,1),name='conv5_pad1'))
+    model.add(Convolution2D(512, 3, 3,name='conv5_act1'))
+    model.add(BatchNormalization(axis=1))
+    model.add(Activation('relu'))
+    model.add(ZeroPadding2D((1,1),name='conv5_pad2'))
+    model.add(Convolution2D(512, 3, 3,name='conv5_act2'))
+    model.add(BatchNormalization(axis=1))
+    model.add(Activation('relu'))
+    model.add(ZeroPadding2D((1,1),name='conv5_pad3'))
+    model.add(Convolution2D(512, 3, 3,name='conv5_act3'))
+    model.add(BatchNormalization(axis=1))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D((2,2), strides=(2,2),name='conv5_max'))
+
+    model.add(Flatten(name='fc_flatten_finetune'))
+    model.add(Dense(4096,name='fc_act1_finetune'))
+    model.add(BatchNormalization(axis=1))
+    model.add(Activation('relu'))
+    model.add(Dropout(0.5,name='fc_dp1_finetune'))
+    model.add(Dense(4096,name='fc_act2_finetune'))
+    model.add(BatchNormalization(axis=1))
+    model.add(Activation('relu'))
+    model.add(Dropout(0.5,name='fc_dp2_finetune'))
+    model.add(Dense(1000,name='fc_max_finetune'))
+    model.add(BatchNormalization(axis=1))
+    model.add(Activation('softmax'))
+
+    model.summary()
+
+    # if weights_path:
+    #     model.load_weights(weights_path, by_name=True)
+
+    return model
---
+++ train2.py
@@ -0,0 +1,61 @@
+import model_q2
+from keras.optimizers import Adam
+from keras.preprocessing.image import ImageDataGenerator
+from keras.callbacks import ModelCheckpoint
+import os
+
+img_width, img_height = 224, 224
+train_data_dir = '/data/datasets/rbonatti/data_processed/2/train'
+validation_data_dir = '/data/datasets/rbonatti/data_processed/2/train20'
+save_models_dir= '/data/datasets/rbonatti/ml_weights2/'
+# nb_train_samples = 7998*2
+nb_train_samples = 1000*4
+nb_validation_samples = 10*4
+epochs = 100
+batch_size = 16
+
+if __name__ == "__main__":
+
+    # Test pretrained model
+    network = model_q2.VGG_16('/data/datasets/rbonatti/vgg16_weights_with_name.h5')
+    adam = Adam(lr=0.001, beta_1=0.9, beta_2=0.999, epsilon=1e-08, decay=0.0)
+    network.compile(optimizer=adam, loss='categorical_crossentropy', metrics=['accuracy'])
+
+    # prepare data augmentation configuration
+    datagen = ImageDataGenerator(rescale=1. / 255)
+
+    train_generator = datagen.flow_from_directory(
+        train_data_dir,
+        target_size=(img_height, img_width),
+        batch_size=batch_size,
+        class_mode='categorical',
+        color_mode='grayscale')
+
+    validation_generator = datagen.flow_from_directory(
+        validation_data_dir,
+        target_size=(img_height, img_width),
+        batch_size=batch_size,
+        class_mode='categorical',
+        color_mode='grayscale')
+
+    # define callbacks
+    # cleanup_callback = LambdaCallback(on_train_end=lambda logs: [p.terminate() for p in processes if p.is_alive()])
+    # plot_loss_callback = LambdaCallback(on_epoch_end=lambda epoch, logs: plt.plot(np.arange(epoch),logs['loss']))
+    # batch_print_callback = LambdaCallback(on_batch_begin=lambda batch,logs: print(batch))
+    # saves the model weights after each epoch if the validation loss decreased
+    checkpointer = ModelCheckpoint(filepath="/data/datasets/rbonatti/ml_weights2/weights.{epoch:02d}-{val_loss:.2f}.hdf5", verbose=1)
+
+    # folder to save the weights
+    if not os.path.exists(save_models_dir):
+        os.makedirs(save_models_dir)
+
+    # fine-tune the model
+    network.fit_generator(
+        generator=train_generator,
+        samples_per_epoch=nb_train_samples,
+        nb_epoch=epochs,
+        validation_data=validation_generator,
+        nb_val_samples=nb_validation_samples,
+        callbacks=[checkpointer]
+        )
+        
