2b864d257430a09095ad7922fbf2eaacc008f74c
Rogerio Bonatti <rbonatti@GS16698.SP.CS.CMU.EDU>
2017-04-16 13:08:09 -0400

    new files

--- rename.py
+++ rename.py
@@ -1,7 +1,7 @@
 import os
 
-for i in range(1):
+for i in range(10000):
 	str_num= '%05d' %i
-	filename_new= '/data/datasets/rbonatti/data_processed/1/valid/'+str_num+'.png'
-	filename_old= '/data/datasets/rbonatti/data_processed/1/valid/'+str(i)+'_orig.png'
+	filename_new= '/data/datasets/rbonatti/data_processed/1/valid/no_class/'+str_num+'.png'
+	filename_old= '/data/datasets/rbonatti/data_processed/1/valid/no_class/'+str(i)+'_orig.png'
 	os.rename(filename_old, filename_new)
