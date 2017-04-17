155e59ed0bb37cc2fb238f051853d5e2c0ab8979
Rogerio Bonatti <rbonatti@GS16698.SP.CS.CMU.EDU>
2017-04-17 10:47:04 -0400

    new files

--- rename2.py
+++ rename2.py
@@ -1,6 +1,7 @@
 import os
 
-for i in range(1000):
+for j in range(1000):
+	i=j+1
 	str_num= '%04d' %i
 	filename_new= '/data/datasets/rbonatti/data_processed/2/train/'+str_num
 	filename_old= '/data/datasets/rbonatti/data_processed/2/train/'+str(i)
