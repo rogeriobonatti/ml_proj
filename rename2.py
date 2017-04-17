181180d2fc1111d58b61987e31ff43249f6c6f3e
Rogerio Bonatti <rbonatti@GS16698.SP.CS.CMU.EDU>
2017-04-17 10:44:21 -0400

    new files

--- rename2.py
+++ rename2.py
@@ -3,5 +3,5 @@ import os
 for i in range(1000):
 	str_num= '%04d' %i
 	filename_new= '/data/datasets/rbonatti/data_processed/2/train/'+str_num
-	filename_old= '/data/datasets/rbonatti/data_processed/1/train/'+str(i)
+	filename_old= '/data/datasets/rbonatti/data_processed/2/train/'+str(i)
 	os.rename(filename_old, filename_new)
